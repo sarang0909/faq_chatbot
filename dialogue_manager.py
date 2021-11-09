1b0759813b64afbb1c70ef1b2076ab882c6f2ce2
Sarang Mete <sarang0909.bds@gmail.com>
2021-11-10 00:18:31 +0530

    Update dialogue_manager.py

--- dialogue_manager.py
+++ dialogue_manager.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from sklearn.metrics.pairwise import cosine_similarity
 
  
 '''dataset preparation
@@ -19,9 +20,8 @@ import tensorflow_hub as hub
 module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
 model = hub.load(module_url)
 def embed(input):
-  return model([input])
+  return np.array(model([input]))
 dataset['Question_Vector'] = dataset.Question.map(embed)
-dataset['Question_Vector'] = dataset.Question_Vector.map(np.array)
 pickle.dump(dataset, open('dataset.pkl', 'wb'))
 '''   
         
@@ -31,8 +31,8 @@ class DialogueManager(object):
         #self.model = tf.saved_model.load("../data/tmp/mobilenet/1/")
         self.model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
         self.dataset = pickle.load(open('dataset.pkl', mode='rb'))
-        self.questions = self.dataset.Question
-        self.QUESTION_VECTORS = np.array(self.dataset.Question_Vector)
+         
+        self.QUESTION_VECTORS =  self.dataset.Question_Vector 
         self.COSINE_THRESHOLD = 0.5
         
         self.chitchat_bot = ChatBot("Chatterbot")       
@@ -42,32 +42,27 @@ class DialogueManager(object):
          
         
     def embed(self,input):
-        return self.model([input])  
-        
-    def cosine_similarity(self,v1, v2):
-        mag1 = np.linalg.norm(v1)
-        mag2 = np.linalg.norm(v2)
-        if (not mag1) or (not mag2):
-            return 0
-        return np.dot(v1, v2) / (mag1 * mag2)
+        return np.array(self.model([input]))         
+    
         
         
-    def semantic_search(self, query, data, vectors):        
-        query_vec = np.array(self.embed(query))
-        res = []
-        for i, d in enumerate(data):
-            qvec = vectors[i].ravel()
-            sim = self.cosine_similarity(query_vec, qvec)
-            res.append((sim, d[:100], i))
-        return sorted(res, key=lambda x : x[0], reverse=True)        
+    def semantic_search(self, query):
+        """Returns max_cos_ind and cosine similairty value
+        query_vec = self.embed(query)
+        sims = []
+        for que_vec  in self.QUESTION_VECTORS:            
+            sim =  cosine_similarity(query_vec, query_vec)
+            sims.append(sim)
+        max_ind = sims.index(max(sims))
+        return max_ind,sims[max_ind][0][0]      
             
     
     def generate_answer(self, question):
         '''This will return list of all questions according to their similarity,but we'll pick topmost/most relevant question'''
-        most_relevant_row = self.semantic_search(question, self.questions, self.QUESTION_VECTORS)[0]
-        print(most_relevant_row)
-        if most_relevant_row[0][0]>=self.COSINE_THRESHOLD:
-            answer = self.dataset.Answer[most_relevant_row[2]]
+        ind, most_relevant_sim_score = self.semantic_search(question)
+         
+        if most_relevant_sim_score >= self.COSINE_THRESHOLD:
+            answer = self.dataset.Answer[ind]
         else:
             answer = self.chitchat_bot.get_response(question)
         return answer
